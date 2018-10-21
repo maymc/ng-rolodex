5dc88c68a1444bd2202dbb0b40376881e2eefa73
May Chen <maychen@Mays-MacBook-Pro.local>
2018-10-20 19:59:17 -1000

    Make header work, add styling

---
+++ ng-rolodex/src/app/pages/pageHeader/pageHeader.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-header',
+  templateUrl: './pageHeader.component.html',
+  styleUrls: ['./pageHeader.component.scss']
+})
+
+export class PageHeaderComponent {
+  title: string = 'Header Component';
+  constructor() {
+
+  }
+}
